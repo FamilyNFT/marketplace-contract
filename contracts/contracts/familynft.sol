f05f992c7287cc5b9e1b2eb74795d6dd12168b17
js3867 <83634235+js3867@users.noreply.github.com>
2023-01-04 16:40:55 +0800

    updated nft factory inc: getMinter() mint() count

--- contracts/contracts/familynft.sol
+++ contracts/contracts/familynft.sol
@@ -4,23 +4,25 @@ pragma solidity ^0.8.0;
 import "@lukso/lsp-smart-contracts/contracts/LSP8IdentifiableDigitalAsset/LSP8IdentifiableDigitalAsset.sol";
 
 contract familynft is LSP8IdentifiableDigitalAsset {
-
+    
+    uint256 count;
+    mapping(bytes32 => string) metadataUri;
     mapping(bytes32 => address) private minter;
 
-    mapping(bytes32 => string) metadataUri;
+    constructor()
+        LSP8IdentifiableDigitalAsset("FAMILYNFT", "FNFT", msg.sender)
+    {}
 
-    constructor() LSP8IdentifiableDigitalAsset("FAMILYNFT", "FNFT", msg.sender) {}
+    function mint(address to, string memory data) public {
+        bytes32 Count = bytes32(count);
+        metadataUri[Count] = data;
+        bytes memory metadata = bytes(data);
+        _mint(to, Count, true, metadata);
+        count += 1;
+    }
 
     function getMetadata(bytes32 tokenId) public view returns (string memory) {
         return metadataUri[tokenId];
-    }
-
-    function mint(address _minter, address to, string memory data) public {
-        bytes32 supply = bytes32(totalSupply() + 1);
-        metadataUri[supply] = data;
-        minter[supply] = _minter;
-        bytes memory metadata = bytes(data);
-        _mint(to, supply, true, metadata);
     }
 
     function getMinter(bytes32 tokenId) public view returns (address) {
