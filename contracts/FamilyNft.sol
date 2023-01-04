ff993e5d59fc5135cd8e89625e43ef1439e4f2bb
js3867 <83634235+js3867@users.noreply.github.com>
2023-01-04 19:35:37 +0800

    several changes made to FamilyNft.sol changed Count to tokenId and included missing minter[tokenId] to populate mapping. Also includes function(s) for testing purposes.

--- contracts/FamilyNft.sol
+++ contracts/FamilyNft.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@lukso/lsp-smart-contracts/contracts/LSP8IdentifiableDigitalAsset/LSP8IdentifiableDigitalAsset.sol";
 
+import "../node_modules/hardhat/console.sol";
+
 contract FamilyNft is LSP8IdentifiableDigitalAsset {
+    uint256 count;
     mapping(bytes32 => address) private minter;
     mapping(bytes32 => string) metadataUri;
 
@@ -12,19 +15,27 @@ contract FamilyNft is LSP8IdentifiableDigitalAsset {
     {}
 
     function mint(address to, string memory data) public {
-        bytes32 Count = bytes32(count);
-        metadataUri[Count] = data;
+        bytes32 tokenId = bytes32(count);
+        minter[tokenId] = to;
+        metadataUri[tokenId] = data;
         bytes memory metadata = bytes(data);
-        _mint(to, Count, true, metadata);
+        _mint(to, tokenId, true, metadata);
         count += 1;
     }
 
     function getMetadata(bytes32 tokenId) public view returns (string memory) {
+        require(_exists(tokenId) == true, "TokenId does not exist");
         return metadataUri[tokenId];
     }
 
     function getMinter(bytes32 tokenId) public view returns (address) {
-        require(_exists(tokenId) == true);
+        require(_exists(tokenId) == true, "TokenId does not exist");
         return minter[tokenId];
+    }
+
+    //////// TESTING FUNCTIONS
+    // suggest change count to getTotalSupply() onlyOwner
+    function getCount() public view onlyOwner returns (uint256) {
+        return count;
     }
 }
